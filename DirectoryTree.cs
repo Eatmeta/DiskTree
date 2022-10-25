a8008b064cf4bb74d887646455bd0db4ee6326c9
Eatmeta <metagroul@gmail.com>
2022-10-25 23:07:09 +0300

    Write a program that restores the directory tree structure from a string

---
+++ DirectoryTree.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiskTree
+{
+    public class DirectoryTree : IEnumerable<DirectoryTree>
+    {
+        public readonly List<DirectoryTree> Branches = new List<DirectoryTree>();
+        public int Level;
+        public string Name;
+        public string Path = "";
+
+        public DirectoryTree(string key)
+        {
+            Name = "ROOT";
+            Level = -1;
+            Branches.Add(new DirectoryTree
+            {
+                Name = key,
+                Level = 0,
+                Path = key + "\\"
+            });
+        }
+
+        public DirectoryTree()
+        {
+            Name = "ROOT";
+            Level = -1;
+        }
+
+        public void Add(string key)
+        {
+            if (Branches.Any(b => b.Name == key))
+                return;
+
+            Branches.Add(new DirectoryTree
+            {
+                Name = key,
+                Level = Level + 1,
+                Path = Path + key + "\\"
+            });
+        }
+
+        public IEnumerator<DirectoryTree> GetEnumerator()
+        {
+            var stack = new Stack<DirectoryTree>();
+            foreach (var item in Branches)
+                stack.Push(item);
+
+            while (stack.Count != 0)
+            {
+                var current = stack.Pop();
+                yield return current;
+
+                foreach (var directoryTree in current.Branches)
+                    stack.Push(directoryTree);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- DiskTreeTask.cs
+++ DiskTreeTask.cs
@@ -1,10 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DiskTree
 {
-    
+    public class DiskTreeTask
+    {
+        public static List<string> Solve(List<string> input)
+        {
+            var tree = ContainTree(input);
+            var treeInOrder = GetTreeInOrder(tree);
+            var d = GetListNamesDirectories(treeInOrder);
+            return GetListNamesDirectories(treeInOrder);
+        }
+
+        private static List<string> GetListNamesDirectories(List<DirectoryTree> treeInOrder)
+        {
+            return treeInOrder.Select(t => new string(' ', t.Level) + t.Name).ToList();
+        }
+
+
+        private static List<DirectoryTree> GetTreeInOrder(DirectoryTree tree)
+        {
+            var list = new List<DirectoryTree>();
+            if (tree.Branches.Count == 0)
+                return list;
+            var stack = new Stack<DirectoryTree>();
+            foreach (var item in tree.Branches.OrderByDescending(t => t.Name, StringComparer.Ordinal))
+                stack.Push(item);
+
+            while (stack.Count != 0)
+            {
+                var current = stack.Pop();
+                list.Add(current);
+                if (current.Branches.Count == 0)
+                    continue;
+                foreach (var item in current.Branches.OrderByDescending(t => t.Name, StringComparer.Ordinal))
+                    stack.Push(item);
+            }
+            return list;
+        }
+
+        private static DirectoryTree ContainTree(List<string> input)
+        {
+            var tree = new DirectoryTree();
+            foreach (var array in input.Select(s => s.Split('\\')))
+            {
+                for (var i = 0; i < array.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        tree.Add(array[i]);
+                        continue;
+                    }
+                    var currentTree = tree.FirstOrDefault(b => b.Path == string.Join("\\", array.Take(i).ToArray())+ "\\");
+                    if (currentTree != null)
+                        currentTree.Add(array[i]);
+                }
+            }
+            return tree;
+        }
+
+        private static DirectoryTree FindTree(string[] array, DirectoryTree tree)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                foreach (var item in tree.Where(t => t.Level == i))
+                {
+                    if (item.Name == array[i])
+                    {
+                        if (FindTree(array.Skip(1).ToArray(), item) == null)
+                            return item;
+                        FindTree(array.Skip(1).ToArray(), item);
+                    }
+                }
+            }
+            return null;
+        }
+    }
 }
